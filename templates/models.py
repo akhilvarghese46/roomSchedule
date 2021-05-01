67a06fbb0fa20c50cab94fde3f351eb941f78757
Akhil varghese <akhil.varghese@student.griffith.ie>
2021-05-01 10:59:56 +0100

    Bracket 1 completed login and logout model for Room,User and booking

---
+++ templates/models.py
@@ -0,0 +1,31 @@
+
+class Room():
+    def __init__(self, name, type, price, req):
+        self.name = name
+        self.type = type
+        self.price = price
+        self.req = req
+
+    def set_properties(self, name, value):
+        setattr(self, name, value)
+
+
+class User():
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def set_properties(self, name, value):
+        setattr(self, name, value)
+
+class Booking():
+    def __init__(self, name, type, price, req, startdate, enddate):
+        self.name = name
+        self.type = type
+        self.price = price
+        self.req = req
+        self.startdate = startdate
+        self.enddate = enddate
+
+    def set_properties(self, name, value):
+        setattr(self, name, value)
